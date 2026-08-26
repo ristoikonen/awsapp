41c2947be27bc1274641c13553a08b74df8aea3b
Risto Ikonen <risto@gmx.com>
2026-08-26 16:31:32 +1000

    User styling

--- src/data/userData.ts
+++ src/data/userData.ts
@@ -21,15 +21,24 @@ export type SampleUser = User;
 
 export const address1 : Address = {
   givenname:"asdaswd" ,
-  familyName: "string",
-  regionCode: "string",
-  postalCode: "string",
-  addressLine: "string",
-  city: "string",
-  administrativeArea: "string",
+  familyName: "grt",
+  regionCode: "2",
+  postalCode: "2912",
+  addressLine: "8 Hallon Street",
+  city: "CBra",
+  administrativeArea: "3",
+};
+export const address2 : Address = {
+  givenname:"Mark" ,
+  familyName: "Smith",
+  regionCode: "2",
+  postalCode: "2911",
+  addressLine: "3 B Eleure Building",
+  city: "CBra",
+  administrativeArea: "2",
 };
 
-export type UserGroup = 'player1';// | 'player2' | 'player3';
+export type UserGroup = 'player1' | 'player2' | 'player3';
 
 export const userGroup1: Record<UserGroup,User> = { 
   player1:
@@ -38,7 +47,25 @@ export const userGroup1: Record<UserGroup,User> = {
     emailAddress:"asdji@yahoo.com",
     phoneNumber:"asdji@yahoo.com"
     ,address:address1
-    ,timeStamp: "1"
+    ,timeStamp: "14:38"
+    ,mobileId:"041"
+  },
+  player2:
+  {
+    userIdentifiers:"GT",
+    emailAddress:"g@nh.com",
+    phoneNumber:"g@nh.com"
+    ,address:address2
+    ,timeStamp: "11:50"
+    ,mobileId:"040"
+  },
+  player3:
+  {
+    userIdentifiers:"XX",
+    emailAddress:"u1@gmai.com",
+    phoneNumber:"u1@gmai.com"
+    ,address:address1
+    ,timeStamp: "18:30"
     ,mobileId:"041"
   }
 };
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -2,23 +2,16 @@
 import { View, Flex } from '@aws-amplify/ui-react';
 // Grid, Card, Flex, Heading, Text, Badge, Button, Image, SearchField, Divider
 import '@aws-amplify/ui-react/styles.css'; 
+import '.././style/table.css'; 
 import {  type UsersProps, type User, userGroup1 } from '../data/userData';
 
-//userGroup1,  type UserGroup,
 
 // , onEdit
-export default function Users({ users }: UsersProps) {
-//: React.FC<UsersProps> = ({ users, onEdit }) => {
+export default function Users({ users, onEdit }: UsersProps) {
 
+const skills: User[] = [userGroup1.player1,userGroup1.player2,userGroup1.player3];
+users = skills;
 
-const skills: User[] = [userGroup1.player1];
-users= skills;
-
-//["TypeScript", "JavaScript", "Node.js"];
-
-  //const [searchQuery, setSearchQuery] = useState<string>('');
-  //const [selectedCategory, setSelectedCategory] = useState<string>('All');
-  //const [selectedImage, setSelectedImage] = useState<ImageAsset | null>(null);
 
   return (
     <View 
@@ -27,7 +20,7 @@ users= skills;
       minHeight="100vh"
     >
       <Flex direction="column" gap="2.5rem" maxWidth="1200px" margin="0 auto">
-        <Flex direction={{ base: 'column', medium: 'row' }} justifyContent="space-between" alignItems="center" gap="1.5rem">
+        <Flex direction={{ base: 'column', medium: 'row' }} justifyContent="space-between" alignItems="left" gap="1.5rem">
         </Flex>
 
           <h2>Google Subscriptions Data</h2>
@@ -40,24 +33,31 @@ users= skills;
                       <th>Address </th>
                       <th>Timestamp</th>
                       <th>Mobile IDs</th>
+                      <th>Action</th>
                   </tr>
               </thead>
               <tbody>
                 {users.map((user) => (
                   <tr>
-                      <td>{user.emailAddress}</td>
+                    <td>{user.mobileId}</td>
+                    <td>{user.emailAddress}</td>
+                    <td>{user.phoneNumber}</td>
+                    <td>{user.address.addressLine}</td>
+                    <td>{user.timeStamp}</td>
+                    <td>{user.mobileId}</td>
+                    
 
                       <td><button
-                      //onClick={() => onEdit(user.userIdentifiers)}
+                      onClick={() => onEdit(user.userIdentifiers)}
                       style={{
                         border: 'none',
                         background: 'transparent',
                         cursor: 'pointer',
-                        fontSize: '1.5rem',
+                        fontSize: '0.9rem',
                       }}
                       aria-label={`Edit user ${user.emailAddress}`}
                     >
-                      &#x270E;
+                      &#x270E; Edit
                     </button></td>
                   </tr>
 
